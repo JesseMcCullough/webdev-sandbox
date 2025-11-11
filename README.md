9e96ee18afd394fcd72b64565780e554a5f47c6d
Jesse McCullough <jesse@jessem.dev>
2025-11-11 18:50:54 -0500

    Add CTA and mobile navigation menu

--- README.md
+++ README.md
@@ -2,11 +2,16 @@
 
 Web development sandbox, just for practice. Learning more about React and Next.js.
 
-
 ## Changes
 
+### November 11, 2025
+
+-   Added CTA to navigation bar
+-   Added mobile menu to navigation bar
+
 ### November 10, 2025
-- Initial setup
-- Added NavBar component
-- Decided on 1200px for max-width container (normally I've used 1900px)
-- Refreshed memory on CSS modules and basic usage of Next.js and React
+
+-   Initial setup
+-   Added NavBar component
+-   Decided on 1200px for max-width container (normally I've used 1900px)
+-   Refreshed memory on CSS modules and basic usage of Next.js and React
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -24,6 +24,19 @@ export default function NavBar() {
                         <a href="#">Item-5</a>
                     </li>
                 </ul>
+                <div className={styles["button-links"]}>
+                    <a href="#" className={styles.cta}>
+                        Button
+                    </a>
+                    <a href="#" className={styles.login}>
+                        Login
+                    </a>
+                </div>
+                <div className={styles["mobile-menu-icon"]}>
+                    <div className={styles.bar}></div>
+                    <div className={styles.bar}></div>
+                    <div className={styles.bar}></div>
+                </div>
             </div>
         </div>
     );
