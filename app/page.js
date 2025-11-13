29f31bae2204e9cd79a629b93a2b3c0f963f479d
Jesse McCullough <jesse@jessem.dev>
2025-11-13 16:24:57 -0500

    Add NavBar component to RootLayout and create Test page

--- app/page.js
+++ app/page.js
@@ -1,34 +1,8 @@
-import {
-    NavBar,
-    NavLink,
-    NavLogo,
-    NavButton,
-} from "../components/NavBar/NavBar";
 import Carousel from "../components/Carousel";
-import logo from "../images/logo.jpg";
 
 export default function Home() {
     return (
         <>
-            <NavBar>
-                <NavLogo image={logo} />
-                <NavLink name="Item-5" href="#">
-                    <NavLink name="SubItem-1" href="#" />
-                    <NavLink name="SubItem-2" href="#" />
-                    <NavLink name="SubItem-3" href="#" />
-                    <NavLink name="SubItem-4" href="#" />
-                    <NavLink name="SubItem-5 Hello Hello" href="#" />
-                </NavLink>
-                <NavLink name="Item-2" href="#" />
-                <NavLink name="Item-3" href="#">
-                    <NavLink name="SubItem-1" href="#" />
-                    <NavLink name="SubItem-2" href="#" />
-                    <NavLink name="SubItem-3" href="#" />
-                </NavLink>
-                <NavLink name="Item-4" href="#" />
-                <NavButton name="Login" href="#" />
-                <NavButton name="Sign Up" href="#" primary={true} />
-            </NavBar>
             <Carousel />
         </>
     );
