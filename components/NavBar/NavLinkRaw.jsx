9889b5f10db2179befacb5a87489368a9d6efa70
Jesse McCullough <jesse@jessem.dev>
2025-11-13 19:05:27 -0500

    Fix mobile dropdown menus and mobile nav bar buttons

--- components/NavBar/NavLinkRaw.jsx
+++ components/NavBar/NavLinkRaw.jsx
@@ -2,22 +2,27 @@
 
 import styles from "./NavBar.module.css";
 import React from "react";
+import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import useIsMobile from "@/hooks/useIsMobile";
 
 export default function NavLinkRaw({ name, href, links = [] }) {
+    const isMobile = useIsMobile();
+    const [showDropdown, setShowDropdown] = useState(false);
     const currentPath = usePathname();
     const isDropdown = links && links.length > 0;
     const isActive = currentPath === href;
 
+    useEffect(() => {
+        setShowDropdown(!isMobile);
+    }, [isMobile]);
+
     return (
         <li>
-            <Link
-                href={href}
-                className={currentPath === href ? styles.active : ""}
-            >
+            <Link href={href} className={isActive ? styles.active : ""}>
                 {name}
-                {isDropdown && (
+                {isDropdown && showDropdown && (
                     <div className={styles["dropdown-icon"]}>
                         <div className={styles.left}></div>
                         <div className={styles.right}></div>
