28051ec27f1c3aeb63d20643fc932675150c60d2
Jesse McCullough <jesse@jessem.dev>
2025-11-24 22:11:05 -0500

    Add SliderArrow component

--- components/Slider/Slider.jsx
+++ components/Slider/Slider.jsx
@@ -10,6 +10,7 @@ export function Slider({ name = null, transitionDuration = 500, children }) {
     const [previousView, setPreviousView] = useState(null);
     const [isTransitioning, setIsTransitioning] = useState(false);
     const timeoutRef = useRef(null);
+    const viewerRef = useRef(null);
 
     function setView(view) {
         clearTimeout(timeoutRef.current);
@@ -31,6 +32,7 @@ export function Slider({ name = null, transitionDuration = 500, children }) {
                 isTransitioning,
                 setIsTransitioning,
                 setView,
+                viewerRef,
             }}
         >
             <div className={name ? `slider ${name}` : "slider"}>{children}</div>
@@ -39,9 +41,8 @@ export function Slider({ name = null, transitionDuration = 500, children }) {
 }
 
 export function SliderViewer({ initialView, renderViewer = null }) {
-    const { currentView, previousView, setView, isTransitioning } =
+    const { currentView, previousView, setView, isTransitioning, viewerRef } =
         useContext(SliderContext);
-    const viewerRef = useRef(null);
 
     useEffect(() => {
         if (!currentView) setView(initialView);
@@ -53,6 +54,7 @@ export function SliderViewer({ initialView, renderViewer = null }) {
                 isTransitioning ? "slider-viewer transition" : "slider-viewer"
             }
             ref={viewerRef}
+            data-index={0}
         >
             <div className="content">
                 {renderViewer ? (
@@ -74,7 +76,13 @@ export function SliderViewer({ initialView, renderViewer = null }) {
 }
 
 export function SliderItems({ children }) {
-    return <div className="slider-items">{children}</div>;
+    return (
+        <div className="slider-items">
+            <SliderArrow direction="left" />
+            <div className="slider-items-container">{children}</div>
+            <SliderArrow direction="right" />
+        </div>
+    );
 }
 
 export function SliderItem({ data, thumbnail }) {
@@ -90,7 +98,42 @@ export function SliderItem({ data, thumbnail }) {
         <div
             className="slider-item"
             onClick={handleClick}
+            onMouseEnter={handleClick}
             style={{ backgroundImage: `url("${thumbnail}")` }}
+            data-value={data}
         ></div>
+    );
+}
+
+export function SliderArrow({ direction }) {
+    const { viewerRef, setView } = useContext(SliderContext);
+
+    function handleClick(e, direction = 1) {
+        const viewerIndex = Number(viewerRef.current.dataset.index);
+        const slider = e.target.closest(".slider");
+        const items = slider.querySelectorAll(".slider-item");
+        const nextIndex = viewerIndex + direction;
+
+        if (nextIndex < 0 || nextIndex >= items.length) return;
+
+        const data = items[nextIndex].dataset.value;
+
+        setView(data);
+
+        viewerRef.current.dataset.index = nextIndex;
+    }
+
+    return (
+        <div
+            className={`slider-arrow ${direction}`}
+            onClick={(e) =>
+                direction === "right" ? handleClick(e) : handleClick(e, -1)
+            }
+        >
+            <div className="slider-arrow-icon">
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
+        </div>
     );
 }
